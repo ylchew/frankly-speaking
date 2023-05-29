10ca6cb795de98f5deafbdd2f08acb02edacdab9
ylchew <113170778+ylchew@users.noreply.github.com>
2023-05-29 14:13:19 +0800

    Create User component

--- app.jsx
+++ app.jsx
@@ -1,5 +1,16 @@
-const App() {
+const App = () => {
+   
+  const User = () => {
+    return(
+      <React.Fragment>
+      <div className="user-small-text">Welcome back,</div>
+      <div className="user-big-text">Lim Ah Gow</div>
+      </React.Fragment>
+     )
+  
   return(
-    <div className="app-interface"></div>
+    <div className="app-interface">
+      <User />
+    </div>
   )
 }
