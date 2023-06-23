eaff369deb426a8b9f03b948bd8c1636d484a8e6
ylchew <113170778+ylchew@users.noreply.github.com>
2023-06-23 16:54:54 +0800

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,6 +1,8 @@
 const App = () => {
-  return(
+  return (
     <div className="app-interface">
+      <h1>Hello, React!</h1>
+      <p>This is a sample React component.</p>
     </div>
-    )
-}
+  );
+};
